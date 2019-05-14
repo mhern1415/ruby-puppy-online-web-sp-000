22145f119886f588b8c8ae9e9adc88688a518bb3
Mark Hernandez <m.hern1415@gmail.com>
2019-05-14 00:37:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,15 +9,17 @@ def initialize(name)
   
 end
 
-def self.all 
-  @@all
-end
 
 def self.all
     @@all.each do |dog|
       puts dog.name
     end
   end
+  
+def self.clear_all
+    @@all.clear
+  end
+
   
   
 end
